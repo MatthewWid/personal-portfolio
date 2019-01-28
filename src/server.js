1d3e62c38d370c4276fb2cba4dd65d157008d208
MatthewWid <115mattey@gmail.com>
2019-01-28 22:43:37 +1100

    Add basic web server, templating, preparatory Pug base layout and homepage and router.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
-// 
+require("dotenv").config({
+	path: "variables.env"
+});
+
+const path = require("path");
+const express = require("express");
+const app = express();
+const routes = require("./routes/index");
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "/views"));
+
+// Set static file directory serving to ../public/
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Set up router that acts on requests to root ('/') and below
+app.use("/", routes);
+
+app.set("port", process.env.PORT || 80);
+const server = app.listen(app.get("port"), () => {
+	console.info(`Now listening on PORT ${app.get("port")}.`);
+});
