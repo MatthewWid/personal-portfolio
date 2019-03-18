ade03d462b8ade8991290e9133d8ad553c576cf9
MatthewWid <115mattey@gmail.com>
2019-03-18 18:16:04 +1100

    Add smooth scroll functionality when clicking logo and navigation links. Add #hero anchor element to hero section.

--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,36 @@ const headerHeight = header.offsetHeight;
 window.addEventListener("scroll", checkScroll);
 
 checkScroll();
+
+// Smooth scroll when clicking header anchor links
+const quadraticEaseInOut = (t) => t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+function animateScroll() {
+	const timeThrough = Math.min(1, (performance.now() - startTime) / duration);
+	window.scroll(0, Math.ceil((quadraticEaseInOut(timeThrough) * (targetScroll - startScroll)) + startScroll));
+
+	if (timeThrough != 1) {
+		requestAnimationFrame(animateScroll);
+	}
+}
+const initScroll = (evt) => {
+	evt.preventDefault();
+
+	startScroll = window.scrollY;
+	startTime = performance.now();
+
+	animateScroll();
+}
+
+const duration = 250;
+let targetScroll, startScroll, startTime;
+// Add event listeners to header logo and navigation items
+document.querySelectorAll("header nav ul li a").forEach((e) => {
+	e.addEventListener("click", (evt) => {
+		targetScroll = document.getElementById(e.href.substring(e.href.indexOf("#") + 1)).offsetTop;
+		initScroll(evt);
+	});
+});
+document.querySelector("header h1 a").addEventListener("click", (evt) => {
+	targetScroll = document.getElementById("hero").offsetTop;
+	initScroll(evt);
+})
