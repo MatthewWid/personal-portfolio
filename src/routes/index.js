df28d1dd11843ad61c5eadf636271d4bd239d429
MatthewWid <115mattey@gmail.com>
2019-04-24 18:16:54 +1000

    Add shortlink routes to social media.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-	res.render("main", {
-		title: "Home"
-	});
-});
+// Main Page
+router.get("/", (req, res) => res.render("main", {title: "Home"}));
+
+// Shortlinks
+router.get("/github", (req, res) => res.redirect("https://github.com/MatthewWid"));
+router.get("/linkedin", (req, res) => res.redirect("https://www.linkedin.com/in/matthew-widdicombe/"));
+router.get("/twitter", (req, res) => res.redirect("https://twitter.com/MatthewWidd"));
+router.get("/codepen", (req, res) => res.redirect("https://codepen.io/MatthewWid/"));
 
 module.exports = router;
