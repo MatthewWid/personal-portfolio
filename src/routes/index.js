3ac4cbc63967d6a42c600db71de9395532f81ab4
MatthewWid <115mattey@gmail.com>
2019-04-25 00:43:58 +1000

    Add resume link route.

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,5 +8,7 @@ router.get("/github", (req, res) => res.redirect("https://github.com/MatthewWid"
 router.get("/linkedin", (req, res) => res.redirect("https://www.linkedin.com/in/matthew-widdicombe/"));
 router.get("/twitter", (req, res) => res.redirect("https://twitter.com/MatthewWidd"));
 router.get("/codepen", (req, res) => res.redirect("https://codepen.io/MatthewWid/"));
+router.get("/resume", (req, res) => res.redirect("https://drive.google.com/file/d/12X6_xCjqTEydhcuvbUOZoGSv3jVTbPO4/view"));
+router.get("/cv", (req, res) => res.redirect("/resume"));
 
 module.exports = router;
