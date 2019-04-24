f1489a33437e920501695e19656349230cce2899
MatthewWid <115mattey@gmail.com>
2019-04-24 18:20:56 +1000

    Add comments to `server.js`.

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const serve_favicon = require("serve-favicon");
 const app = express();
 const routes = require("./routes/index");
 
+// Pug view engine setup
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -17,9 +18,11 @@ app.use(express.static(path.join(__dirname, "../public")));
 // Favicon
 app.use(serve_favicon(path.join(__dirname, "../public/favicon.ico")));
 
-// Set up router that acts on requests to root ('/') and below
+// Routes
 app.use("/", routes);
+
+// Set port to listen on
 app.set("port", process.env.PORT || 80);
-const server = app.listen(app.get("port"), () => {
-	console.info(`Express server listening on PORT ${app.get("port")}. (${server.address().address})`);
-});
+
+// Start server
+const server = app.listen(app.get("port"), () => console.info(`Express server listening on PORT ${app.get("port")}. (${server.address().address})`));
