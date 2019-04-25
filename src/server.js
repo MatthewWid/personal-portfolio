6438da28fc95f8c0ee65f9d37439ed84db3585c6
MatthewWid <115mattey@gmail.com>
2019-04-26 00:37:55 +1000

    Simplify comments in server.js.

--- src/server.js
+++ src/server.js
@@ -8,11 +8,11 @@ const serve_favicon = require("serve-favicon");
 const app = express();
 const routes = require("./routes/index");
 
-// Pug view engine setup
+// View engine
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views"));
 
-// Set static file directory serving to ../public/
+// Static files
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Favicon
@@ -21,8 +21,8 @@ app.use(serve_favicon(path.join(__dirname, "../public/favicon.ico")));
 // Routes
 app.use("/", routes);
 
-// Set port to listen on
+// Port
 app.set("port", process.env.PORT || 80);
 
-// Start server
+// Server
 const server = app.listen(app.get("port"), () => console.info(`Express server listening on PORT ${app.get("port")}. (${server.address().address})`));
