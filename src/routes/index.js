a7f96c68dc1054e0c1d434a8295efbdd39f282d8
MatthewWid <matthew.widdi@gmail.com>
2020-02-24 21:06:58 +1100

    Update router for resume/CV to use the same route handler, instead of having an unnecessary redirect.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,13 @@
 const router = require("express").Router();
 
 // Main Page
-router.get("/", (req, res) => res.render("main"));
+router.get("/", (_, res) => res.render("main"));
 
 // Shortlinks
-router.get("/github", (req, res) => res.redirect("https://github.com/MatthewWid"));
-router.get("/linkedin", (req, res) => res.redirect("https://www.linkedin.com/in/matthew-widdicombe/"));
-router.get("/twitter", (req, res) => res.redirect("https://twitter.com/MatthewWidd"));
-router.get("/codepen", (req, res) => res.redirect("https://codepen.io/MatthewWid"));
-router.get("/resume", (req, res) => res.redirect("https://drive.google.com/file/d/1gdcjk3jynA8Kt6ie7Kev1gBmLGcs8hQe/view"));
-router.get("/cv", (req, res) => res.redirect("/resume"));
+router.get("/github", (_, res) => res.redirect("https://github.com/MatthewWid"));
+router.get("/linkedin", (_, res) => res.redirect("https://www.linkedin.com/in/matthew-widdicombe/"));
+router.get("/twitter", (_, res) => res.redirect("https://twitter.com/MatthewWidd"));
+router.get("/codepen", (_, res) => res.redirect("https://codepen.io/MatthewWid"));
+router.get(["/resume", "/cv"], (_, res) => res.redirect("https://drive.google.com/file/d/1gdcjk3jynA8Kt6ie7Kev1gBmLGcs8hQe/view"));
 
 module.exports = router;
