a3eadf4907f174258e07751420c68aabbd8b4c21
MatthewWid <matthew.widdi@gmail.com>
2020-03-01 23:16:45 +1100

    Add TypeScript compilation to base webpack configuration.

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -2,6 +2,25 @@ const path = require("path");
 
 const BASEDIR = path.resolve(__dirname, "../");
 
-const config = {};
+const config = {
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: [
+					{
+						loader: "ts-loader",
+						options: {
+							configFile: path.join(BASEDIR, "./tsconfig.json"),
+						},
+					},
+				],
+			},
+		],
+	},
+};
 
 module.exports = config;
