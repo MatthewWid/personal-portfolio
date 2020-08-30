aa9da9f411971c2432f47fdd4f4bd522590dbbbd
Matthew W <matthew.widdi@gmail.com>
2020-08-30 15:47:05 +1000

    Add '/.vimrc' route that fetches my own vimrc file from Dropbox in real time.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,3 +1,4 @@
+const fetch = require("node-fetch");
 const router = require("express").Router();
 
 // Pages
@@ -9,5 +10,26 @@ router.get("/linkedin", (_, res) => res.redirect("https://www.linkedin.com/in/ma
 router.get("/twitter", (_, res) => res.redirect("https://twitter.com/MatthewWidd"));
 router.get("/codepen", (_, res) => res.redirect("https://codepen.io/MatthewWid"));
 router.get(["/resume", "/cv"], (_, res) => res.redirect("https://drive.google.com/file/d/1gdcjk3jynA8Kt6ie7Kev1gBmLGcs8hQe/view"));
+
+// Integrations
+router.get("/.vimrc", async (_, res) => {
+	const response = await fetch(
+		"https://content.dropboxapi.com/2/files/download",
+		{
+			method: "POST",
+			headers: {
+				Authorization: `Bearer ${process.env.DROPBOX_ACCESS_TOKEN}`,
+				"Dropbox-API-Arg": `{\"path\": \"${process.env.DROPBOX_VIMRC_PATH}\"}`
+			}
+		}
+	);
+
+	if (response.ok) {
+		const body = await response.text();
+		res.send(body);
+	} else {
+		res.status(404).send("Failed to fetch .vimrc from Dropbox.");
+	}
+});
 
 module.exports = router;
