3d9518750e94f9f998f3cda51c062d6db3c42e54
Matthew W <matthew.widdi@gmail.com>
2020-11-12 14:59:08 +1100

    Update dropboxFile to be able to download arbitrary files instead of explicitely named ones.

--- src/server/middleware/dropboxFile.js
+++ src/server/middleware/dropboxFile.js
@@ -1,14 +1,20 @@
 const path = require("path");
 const fetch = require("node-fetch");
 
-const dropboxFile = (filePath) => async (_, res) => {
+const dropboxFile = async (req, res) => {
+	const rawPath = req.params[0];
+
+	const safePath = path.join("/", rawPath);
+
+	const fullPath = path.join(process.env.DROPBOX_BASE_PATH, safePath);
+
 	const response = await fetch(
 		"https://content.dropboxapi.com/2/files/download",
 		{
 			method: "POST",
 			headers: {
 				Authorization: `Bearer ${process.env.DROPBOX_ACCESS_TOKEN}`,
-				"Dropbox-API-Arg": `{\"path\": \"${filePath}\"}`
+				"Dropbox-API-Arg": `{\"path\": \"${fullPath}\"}`
 			}
 		}
 	);
@@ -16,9 +22,10 @@ const dropboxFile = (filePath) => async (_, res) => {
 	if (response.ok) {
 		const body = await response.text();
 		res.type("text/plain").send(body);
+		console.info("User downloaded file", fullPath);
 	} else {
-		res.status(404).send(`Failed to fetch ${path.basename(filePath)} from Dropbox.`);
-		console.error(response);
+		res.status(404).send(`Failed to fetch "${path.basename(rawPath)}", it may be the wrong path or may not exist.`);
+		console.error("User failed to download file", fullPath);
 	}
 };
 
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -11,10 +11,7 @@ router.get("/twitter", (_, res) => res.redirect("https://twitter.com/MatthewWidd
 router.get("/codepen", (_, res) => res.redirect("https://codepen.io/MatthewWid"));
 router.get(["/resume", "/cv"], (_, res) => res.redirect("https://drive.google.com/file/d/1gdcjk3jynA8Kt6ie7Kev1gBmLGcs8hQe/view"));
 
-// Config Files
-router.get("/.vimrc", dropboxFile(process.env.DROPBOX_VIMRC_PATH));
-router.get("/.bash_aliases", dropboxFile(process.env.DROPBOX_BASH_ALIASES_PATH));
-router.get("/.bashrc", dropboxFile(process.env.DROPBOX_BASHRC_PATH));
-router.get("/.tmux.conf", dropboxFile(process.env.DROPBOX_TMUX_CONF_PATH));
+// Dotfiles
+router.get("/dl/*", dropboxFile);
 
 module.exports = router;
